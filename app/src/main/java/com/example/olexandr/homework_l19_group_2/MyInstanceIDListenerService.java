8b99899464922f611e98a969b5069b11d4d0cdca
AlexVarchuk <82346920f>
2015-09-10 23:53:12 +0300

    Connect GSM

---
+++ app/src/main/java/com/example/olexandr/homework_l19_group_2/MyInstanceIDListenerService.java
@@ -0,0 +1,9 @@
+package com.example.olexandr.homework_l19_group_2;
+
+import com.google.android.gms.iid.InstanceIDListenerService;
+
+
+public class MyInstanceIDListenerService extends InstanceIDListenerService {
+
+
+}
