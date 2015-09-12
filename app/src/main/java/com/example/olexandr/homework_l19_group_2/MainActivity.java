2b88fb228834aef874619a4de24966acf8c0ce35
AlexVarchuk <82346920f>
2015-09-12 23:10:28 +0300

    saved by 12.09.2015

--- app/src/main/java/com/example/olexandr/homework_l19_group_2/MainActivity.java
+++ app/src/main/java/com/example/olexandr/homework_l19_group_2/MainActivity.java
@@ -1,37 +1,149 @@
 package com.example.olexandr.homework_l19_group_2;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v4.app.NotificationCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.olexandr.homework_l19_group_2.adater.AdapterNotification;
+import com.example.olexandr.homework_l19_group_2.database.BaseDataNotification;
+import com.example.olexandr.homework_l19_group_2.model.ItemNotification;
+import com.google.android.gms.gcm.GoogleCloudMessaging;
+import com.google.android.gms.iid.InstanceID;
+
+import java.io.IOException;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    public static final String TAG = MainActivity.class.getSimpleName();
+
+    private Button mClearList;
+    private Button mUpdate;
+    private Button mClearDatabase;
+    private ListView mNotificationList;
+
+    private AdapterNotification mAdapter;
+    private List<ItemNotification> mData;
+    private NotificationCompat.Builder mBuilder;
+    private NotificationManager mNotificationManager;
+    private BaseDataNotification mBaseDataNotification;
+    private SQLiteDatabase mSQLiteDatabase;
+    private static final int NOTIFICATION_ID = 1234;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mNotificationList = (ListView) findViewById(R.id.lv_notification_AM);
+        mClearDatabase = (Button) findViewById(R.id.btn_clear_database_AM);
+        mClearList = (Button) findViewById(R.id.btn_clear_list_AM);
+        mUpdate = (Button) findViewById(R.id.btn_update_list_AM);
+
+        mClearDatabase.setOnClickListener(this);
+        mClearList.setOnClickListener(this);
+        mUpdate.setOnClickListener(this);
+
+        mBaseDataNotification = new BaseDataNotification(this, "mydata.db", null, 1 );
+        mSQLiteDatabase  = mBaseDataNotification.getReadableDatabase();
+
+        mAdapter = new AdapterNotification(this, mData);
+        mNotificationList.setAdapter(mAdapter);
+        mNotificationList.setOnClickListener(this);
+
+
+
+
+
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    protected void onResume() {
+        super.onResume();
+
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btnAddNotification_AL19:
+//                onClickAddNotification();
+                getToken();
+                break;
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+            case R.id.btnUpdateNotification_AL19:
+                onClickUpdateNotification();
+                break;
+
+            case R.id.btnRemoveNotification_AL19:
+                onClickRemoveNotification();
+                break;
         }
-
-        return super.onOptionsItemSelected(item);
     }
+
+    private void getToken() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    InstanceID instanceID = InstanceID.getInstance(MainActivity.this);
+                    String token = instanceID.getToken(getString(R.string.gcm_defaultSenderId),
+                            GoogleCloudMessaging.INSTANCE_ID_SCOPE, null);
+                    Log.d(TAG, "Token: " + token);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+
+    private void onClickAddNotification() {
+        mBuilder = new NotificationCompat.Builder(this)
+                .setSmallIcon(android.R.drawable.ic_dialog_alert)
+                .setLargeIcon(BitmapFactory.decodeResource(getResources(), android.R.drawable.sym_def_app_icon))
+                .setContentTitle("My notification")
+                .setContentText("Hello World!")
+                .setAutoCancel(true);
+
+        Intent resultIntent = new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, NOTIFICATION_ID, resultIntent, PendingIntent.FLAG_CANCEL_CURRENT);
+
+        mBuilder.setContentIntent(pendingIntent);
+
+//        mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+//        mNotificationManager.notify(NOTIFICATION_ID, mBuilder.build());
+
+        Notification notification = mBuilder.build();
+        notification.ledOnMS = 100;
+        notification.ledOffMS = 100;
+        notification.flags = Notification.FLAG_SHOW_LIGHTS;
+
+        mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        mNotificationManager.notify(NOTIFICATION_ID, notification);
+    }
+
+    private void onClickUpdateNotification() {
+        if (mBuilder == null || mNotificationManager == null) return;
+
+        mBuilder.setContentText("Updated: " + System.currentTimeMillis() % 10000);
+        mNotificationManager.notify(NOTIFICATION_ID, mBuilder.build());
+    }
+
+    private void onClickRemoveNotification() {
+        if (mNotificationManager == null) return;
+
+        mNotificationManager.cancel(NOTIFICATION_ID);
+    }
+
 }
--- app/src/main/java/com/example/olexandr/homework_l19_group_2/MyInstanceIDListenerService.java
+++
@@ -1,9 +0,0 @@
-package com.example.olexandr.homework_l19_group_2;
-
-import com.google.android.gms.iid.InstanceIDListenerService;
-
-
-public class MyInstanceIDListenerService extends InstanceIDListenerService {
-
-
-}
